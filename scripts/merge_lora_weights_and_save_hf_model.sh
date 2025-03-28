f1de1e7b2fd3e1256a34cfc5433354560b642dee
Magic-Xin <mail@magicxin.tech>
2025-03-28 15:24:47 -0400

    fixed typo

--- scripts/merge_lora_weights_and_save_hf_model.sh
+++ scripts/merge_lora_weights_and_save_hf_model.sh
@@ -5,6 +5,6 @@ python src/utils/merge_lora_weights_and_save_hf_model.py \
     --model_type vlm_qwen \
     --mm_projector_type "mixer" \
     --pretrain_vision_model ./output/DCFormer_SigLIP/pretrained_ViT.bin \
-    --vision_tower "decomposed" \
+    --vision_tower "dcformer" \
     --model_with_lora ./output/Med3DVLM-Qwen-2.5-7B-finetune/model_with_lora.bin \
     --output_dir ./models/Med3DVLM-Qwen-2.5-7B
